cac44d89ac5459d0e7d0fc0e36ef54bc7e1979a8
Karabo Mashao <karabomashao1@gmail.com>
2025-03-16 23:45:19 +0200

    Creating service for categories

---
+++ src/main/java/com/example/enviro365_/service/CategoryService.java
@@ -0,0 +1,44 @@
+package com.example.enviro365_.service;
+
+import com.example.enviro365_.model.Category;
+import com.example.enviro365_.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public Category findCategoryById(int id){
+        return categoryRepository.findById(id).orElseThrow(() -> new NullPointerException("Id not found"));
+    }
+
+    public void saveCategory(Category category){
+        categoryRepository.save(category);
+    }
+
+    public void updateCategoryById(int id, Category category){
+        Category existingCategory = findCategoryById(id);
+        existingCategory.setDescription(category.getDescription());
+        existingCategory.setTypeOfCategory(category.getTypeOfCategory());
+    }
+
+    public void deleteCategoryById(int id){
+        categoryRepository.deleteById(id);
+    }
+
+    public void deleteAllCategories(){
+        categoryRepository.deleteAll();
+    }
+}
