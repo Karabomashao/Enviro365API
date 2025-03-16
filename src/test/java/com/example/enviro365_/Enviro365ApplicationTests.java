6c5c9da502d5034184f148fcba63cb04c2cf5e2f
Karabo Mashao <karabomashao1@gmail.com>
2025-03-16 22:29:19 +0200

    Fixing file structure

---
+++ src/test/java/com/example/enviro365_/Enviro365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enviro365_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enviro365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
